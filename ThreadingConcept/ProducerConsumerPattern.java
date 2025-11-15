3f4ffdf3a11e11658f7663a3d2219cd7975c3821
evhook <anonymous1986222@gmail.com>
2025-11-15 21:11:05 +0530

    Add Producer-Consumer pattern implementation using BlockingQueue 🚀

---
+++ ThreadingConcept/ProducerConsumerPattern.java
@@ -0,0 +1,34 @@
+package ThreadingConcept;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+public class ProducerConsumerPattern {
+    public static void main(String[] args){
+        BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(5);
+        Thread producer  = new Thread(()->{
+            int value=1;
+            try{
+                while(true){
+                    System.out.println("Producer produced : "+value);
+                    queue.put(value);
+                    value++;
+                    Thread.sleep(500);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        });
+        Thread consumer = new Thread(()->{
+            try{
+                while(true){
+                    int data= queue.take();
+                    System.out.println("Consumer consumed: "+data);
+                    Thread.sleep(1000);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        });
+        producer.start();
+        consumer.start();
+    }
+}
