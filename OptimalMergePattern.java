6fe43e95e31436bf3727227c69d1081e8d6f66e6
evhook <anonymous1986222@gmail.com>
2025-12-05 20:27:44 +0530

    Refactor OptimalMergePattern to use Threadize class for computation in a separate thread

--- OptimalMergePattern.java
+++ OptimalMergePattern.java
@@ -1,17 +1,40 @@
 import java.util.*;
-public class OptimalMergePattern {
-    public static void main(String[] args){
-        int files[] = new int[]{2,4,5,1,9,20};
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i=0;i<files.length;i++){
-            pq.add(files[i]);
+
+class Threadize extends Thread{
+    private int count;
+    private PriorityQueue<Integer> pq;
+        public Threadize(PriorityQueue<Integer> pq){
+            this.pq=pq;
         }
-        int count = 0;
-        while(pq.size()>1){
+         @Override
+        public void run(){
+        try{
+            while(pq.size()>1){
             int temp = pq.poll()+pq.poll();
             count+=temp;
-            pq.add(temp);
+                pq.add(temp);
+            // Thread.sleep(1000);
+         }
+        }catch(Exception e){
+            
         }
-        System.out.println("Minimum Computation : "+count);
+    }
+    public int valueOfCount(){
+        return count;
+    }
+       
+        
+}
+public class OptimalMergePattern {
+    public static void main(String[] args)throws InterruptedException{
+             int files[] = new int[]{28,32,12,5,84,53,91,35,3,11};
+         PriorityQueue<Integer> pq = new PriorityQueue<>();
+         for(int i=0;i<files.length;i++){
+             pq.add(files[i]);
+            }
+            Threadize thread = new Threadize(pq);
+            thread.start();
+            thread.join();
+        System.out.println("Minimum Computation : "+thread.valueOfCount());
     }
 }
