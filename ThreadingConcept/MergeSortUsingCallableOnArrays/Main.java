99bf87ca57e10e6184b6c1fe177bf5e1a724c318
evhook <anonymous1986222@gmail.com>
2025-12-21 16:40:44 +0530

    Change ExecutorService to use a fixed thread pool for improved performance

--- ThreadingConcept/MergeSortUsingCallableOnArrays/Main.java
+++ ThreadingConcept/MergeSortUsingCallableOnArrays/Main.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Future;
 public class Main {
     public static void main(String[] args)throws Exception{
         int arr[] = {38, 27, 43, 3, 9, 82, 10};
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         Future<int[]> future = executorService.submit(new LogicPart(arr));
         int[] sorted =  future.get();
         executorService.shutdown();
