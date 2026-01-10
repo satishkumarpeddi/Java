5c9d75551363dbfb63357c6d5a8ea07581486aa9
evhook <anonymous1986222@gmail.com>
2026-01-10 18:46:03 +0530

    Add comments to clarify dynamic programming table initialization in EqualingStringWithMinimumCost class

--- DynamicProgramming/EqualingStringWithMinimumCost.java
+++ DynamicProgramming/EqualingStringWithMinimumCost.java
@@ -8,9 +8,9 @@ public class EqualingStringWithMinimumCost {
         int n = s2.length();
         int dp[][] = new int[m+1][n+1];
         for(int i=m-1;i>=0;i--)
-            dp[i][n] = dp[i+1][n]+s1.charAt(i);
+            dp[i][n] = dp[i+1][n]+s1.charAt(i);//col arrangement
         for(int j=n-1;j>=0;j--)
-            dp[m][j] = dp[m][j+1]+s2.charAt(j);
+            dp[m][j] = dp[m][j+1]+s2.charAt(j);//row arrangement
         for(int i=m-1;i>=0;i--)
             for(int j=n-1;j>=0;j--)
                 if(s1.charAt(i)==s2.charAt(j))
