e00d72566e0d59506f52d29e1614c302e849ac75
evhook <anonymous1986222@gmail.com>
2026-01-25 17:37:16 +0530

    Add comment to clarify purpose of FastScanner class in Main.java

--- FastScanner/Main.java
+++ FastScanner/Main.java
@@ -3,6 +3,8 @@ import java.io.*;
 //Scanner Class ===> Slower
 //BufferedReader ===> Intermediate
 //FastScanner Class ===> Faster
+
+//FastScanner for better input handle 
 public class Main{
     public static void main(String[] args)throws IOException{
 
