edc53654c657c84461009eaf04a48e76229d1353
evhook <anonymous1986222@gmail.com>
2026-01-29 20:34:04 +0530

    Refactor ReplaceCharacterArray to use Queue and HashMap for replacing consecutive duplicates

--- ReplaceCharacterArray.java
+++ ReplaceCharacterArray.java
@@ -1,9 +1,21 @@
+import java.util.*;
 public class ReplaceCharacterArray {
   public static void main(String[] args){
-    char arr[] = "aabbcced".toCharArray();
-    for(int  i=1;i<arr.length;i++)
-        if(arr[i]==arr[i-1])
-            arr[i]='#';
-    System.out.println(String.valueOf(arr));
+    String str = "aabc";
+    Queue<Character> queue = new LinkedList<>();
+    HashMap<Character,Integer> map = new HashMap<>();
+    StringBuilder resultString = new StringBuilder();
+    for(int i=0;i<str.length();i++){
+        char ch = str.charAt(i);
+        queue.add(ch);
+        map.put(ch,map.getOrDefault(ch,0)+1);
+        while(!queue.isEmpty()&&map.get(queue.peek())>1)
+            queue.poll();
+        if(queue.isEmpty())
+            resultString.append('#');
+        else
+            resultString.append(queue.peek());
+    }
+    System.out.println("The result string : "+resultString.toString());
   }   
 }
