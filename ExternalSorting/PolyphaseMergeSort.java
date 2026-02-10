e49bb7fb7c3445da1763a8ec757e14b1eb0cf948
evhook <anonymous1986222@gmail.com>
2026-02-10 06:39:22 +0530

    Remove unused classes and implement merge method in PolyphaseMergeSort

--- ExternalSorting/PolyphaseMergeSort.java
+++ ExternalSorting/PolyphaseMergeSort.java
@@ -20,9 +20,28 @@ public class PolyphaseMergeSort {
             int outIdx = tapes.indexOf(tapes.stream().min(Comparator.comparingInt(List::size)).get());
             List<Integer> in1 = new ArrayList<>(tapes.get((outIdx+1)%3));
             List<Integer> in2 = new ArrayList<>(tapes.get((outIdx+2)%3));
-            tapes.set(outIdx,merge(in1,in2))
+            tapes.set(outIdx,merge(in1,in2));
 
-        }   
+        }
+        return tapes.stream().flatMap(List::stream).collect(java.util.stream.Collectors.toList());
+    }
+    public static List<Integer> merge(List<Integer> list1, List<Integer> list2){
+        List<Integer> merged = new ArrayList<>();
+        int i = 0, j = 0;
+        while(i < list1.size() && j < list2.size()){
+            if(list1.get(i) <= list2.get(j)){
+                merged.add(list1.get(i++));
+            } else {
+                merged.add(list2.get(j++));
+            }
+        }
+        while(i < list1.size()){
+            merged.add(list1.get(i++));
+        }
+        while(j < list2.size()){
+            merged.add(list2.get(j++));
+        }
+        return merged;
     }
     public static void main(String[] args){
         List<Integer> data = Arrays.asList(5,2,4,6,1,3,8,7,9);
